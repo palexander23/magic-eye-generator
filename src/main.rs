942a19d64c3e04f473bd1f3b3550508f5910dcfc
Pete Alexander <peter.d.alexander23@gmail.com>
2022-07-12 23:33:55 +0100

    Add openning blank window

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub fn run() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow,| {
+        match event{
+            Event::WindowEvent { ref event, window_id } => {
+                
+                // Check we're examining the correct window
+                if window.id() != window_id{
+                    return;
+                }
+
+                // Handle all window events
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+
+                    _ => {},
+                }                 
+            }
+            _ => {},
+        }
+    });
+}
+
+fn main() {
+    std::env::set_var("WINIT_UNIX_BACKEND", "x11");
+
+    run();
+}
