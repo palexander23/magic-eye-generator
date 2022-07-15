7f07997d73d3e46ef5a82f7a08be80d5fe8e689f
Pete Alexander <peter.d.alexander23@gmail.com>
2022-07-15 09:27:15 +0100

    Move existing application code to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub fn run_window() {
+    std::env::set_var("WINIT_UNIX_BACKEND", "x11");
+
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            // Perform first time setup for the event loop
+            Event::NewEvents(StartCause::Init) => {
+                *control_flow = ControlFlow::Wait;
+            }
+
+            Event::WindowEvent { ref event, window_id } => {
+                // Check we're examining the correct window
+                if window.id() != window_id {
+                    return;
+                }
+
+                // Handle window close button pressed
+                if event == &WindowEvent::CloseRequested {
+                    *control_flow = ControlFlow::Exit
+                }
+            }
+            _ => {}
+        }
+    });
+}
